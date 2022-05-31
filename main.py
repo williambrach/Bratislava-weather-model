1833a853dab502180ca6504d38f6e631a92e0ddb
williambrach <wibrach@gmail.com>
2022-05-31 18:29:29 +0200

    time fix

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def getData(sc):
         url = "http://www.shmu.sk/sk/?page=59"
         response = requests.get(url)
         soup = BeautifulSoup(response.text, "html.parser")
-        time = datetime.now().strftime("%Y/%m/%d %H:%M")
+        timeNow = datetime.now().strftime("%Y/%m/%d %H:%M")
         for row in soup.findAll("table")[0].find_all("tr"):
             columns = row.find_all("td")
             if columns[0].text not in (
@@ -47,7 +47,7 @@ def getData(sc):
                 1,
                 number=1,
                 values=[
-                    time,
+                    timeNow,
                     station,
                     temperature,
                     wind_dir,
@@ -61,20 +61,20 @@ def getData(sc):
             )
         sc.enter(SECONDS, 1, getData, (sc,))
     except Exception as e:
-        time = datetime.now().strftime("%Y/%m/%d %H:%M")
-        e_msg = f"{time} - Bratislava weather - {str(e)}"
+        timeErr = datetime.now().strftime("%Y/%m/%d %H:%M")
+        e_msg = f"{timeErr} - Bratislava weather - {str(e)}"
         client.sendMessage(TELEGRAM_USER, e_msg)
         sc.enter(SECONDS, 1, getData, (sc,))
 
 
 def main():
+    s = sched.scheduler(time.time, time.sleep)
     try:
-        s = sched.scheduler(time.time, time.sleep)
         s.enter(SECONDS, 1, getData, (s,))
         s.run()
     except Exception as e:
-        time = datetime.now().strftime("%Y/%m/%d %H:%M")
-        e_msg = f"{time} - Bratislava weather - {str(e)}"
+        timeErr = datetime.now().strftime("%Y/%m/%d %H:%M")
+        e_msg = f"{timeErr} - Bratislava weather - {str(e)}"
         client.sendMessage(TELEGRAM_USER, e_msg)
         
 
